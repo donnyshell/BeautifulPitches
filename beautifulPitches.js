34ff2b6de6504687bd12aa1d27078c29a65f152b
donnyshell <jdsheld@gmail.com>
2023-07-10 00:21:10 -0600

    remove old header

--- beautifulPitches.js
+++ beautifulPitches.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         Beautiful Pitches
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  This script makes moutnain project pitch descriptions beautiful
-// @author       Jonah
-// @match        https://www.mountainproject.com/route/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=mountainproject.com
-// @grant        none
-// ==/UserScript==
-
 function getNumberOfPitches(){
     let routeDescription = document.getElementsByClassName("description-details")[0].rows[0].cells[1];
     let numberOfPitches = parseInt(routeDescription.innerText.match(/\d* pitches/));
@@ -36,24 +25,23 @@ function createTable(pitchDescriptionArray){
             }
         }
     }
-    routeDescription.insertAdjacentHTML('afterend', pitchTable.outerHTML);
+    return pitchTable;
 }
 
-(function() {
-    'use strict';
-
-    // Your code here...
+function beautifulPitches() {
+    //get number of pitches, run if multipitch
     let numberOfPitches = GetNumberOfPitches();
     if(!numberOfPitches){
         return 0;
     }
 
-
     //parse the description, break it up and pass it to create table
-    let routeDescription = document.getElementsByClassName("fr-view")[0];
-    let pitchDescriptionArray = routeDescription.innerText.split('\n').filter(pitch => pitch.match(/^P\d/));
+    1let pitchDescriptionArray = routeDescription.innerText.split('\n').filter(pitch => pitch.match(/^P\d/));
     if( pitchDescriptionArray.length === numberOfPitches){
-        createTable(pitchDescriptionArray);
+        var pitchTable = createTable(pitchDescriptionArray);
     }
+    routeDescription.insertAdjacentHTML('afterend', pitchTable.outerHTML);
+};
 
-})();
+const routeDescription = document.getElementsByClassName("fr-view")[0];
+beautifulPitches();
